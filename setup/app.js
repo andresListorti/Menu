fb13d33dc6dd46a927006637f7c20106571bf222
andresListorti <andreslistorti@gmail.com>
2022-04-20 17:05:16 -0300

    third commit

--- setup/app.js
+++ setup/app.js
@@ -71,45 +71,28 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
-
-const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn')
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
 
 // cargar items
-window.addEventListener('DOMContentLoaded', function(){
-  displayMenuItems(menu)
-
-  
-})
-
-//filter items
-
-filterBtns.forEach(function(btn){
-  btn.addEventListener('click', function(e){
-    const categoria = e.currentTarget.dataset.pepito;
-    const menuCategoria = menu.filter(function(menuItem){
-      // console.log(menuItem.category);
-      if(menuItem.category === categoria){
-        return menuItem
-        }
-     })
-    // console.log(menuCategoria); el array con los obj q tienen esa category
-    // console.log(categoria); la categoria que le pusimos al dateset-
-    if(categoria === 'all'){
-      displayMenuItems(menu)
-    } else {
-      displayMenuItems(menuCategoria)
-    }
-  })
-})
-
-
-
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
 
 function displayMenuItems(menuItems) {
-let displayMenu = menuItems.map(item => `<article class="menu-item">
+  let displayMenu = menuItems.map(
+    (item) => `<article class="menu-item">
           <img src=${item.img} class="photo" alt=${item.title} />
           <div class="item-info">
             <header>
@@ -120,8 +103,49 @@ let displayMenu = menuItems.map(item => `<article class="menu-item">
               ${item.desc}
             </p>
           </div>
-        </article>`)
-        displayMenu = displayMenu.join('') // esto elimina las comas en el medio y lo hace un gran string
-        sectionCenter.innerHTML = displayMenu
+        </article>`
+  );
+  displayMenu = displayMenu.join(""); // esto elimina las comas en el medio y lo hace un gran string
+  sectionCenter.innerHTML = displayMenu;
+}
 
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (valor, item) {
+      if (!valor.includes(item.category)) {
+        valor.push(item.category);
+      }
+      return valor;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button" data-pepito=${category}>
+    ${category}
+    </button>`;
+    })
+    .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  //filter items
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const categoria = e.currentTarget.dataset.pepito;
+      const menuCategoria = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === categoria) {
+          return menuItem;
+        }
+      });
+      // console.log(menuCategoria); el array con los obj q tienen esa category
+      // console.log(categoria); la categoria que le pusimos al dateset-
+      if (categoria === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategoria);
+      }
+    });
+  });
 }
